1e7b7ab3fa6170acfe66c5ba1b5fa7874c2f78e3
Willyan Cirino <wcirino@fazenda.rj.gov.br>
2024-05-07 20:58:16 -0300

    Adicionando projeto

---
+++ simuladorEmail/src/test/java/simuladorEmail/simuladorEmail/SimuladorEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package simuladorEmail.simuladorEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ simuladorPagamento/src/main/java/simuladorPagamento/SimuladorPagamentoApplication.java
@@ -0,0 +1,13 @@
+package simuladorPagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorPagamentoApplication.class, args);
+	}
+
+}
