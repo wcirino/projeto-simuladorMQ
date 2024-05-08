e550fee6ae18c750c7ea90375ee6ed0a44aae55f
Willyan Cirino <wcirino@fazenda.rj.gov.br>
2024-05-07 21:26:48 -0300

    Add projeto certo maven

---
+++ simuladorPagamento/src/test/java/simuladorPagamento/SimuladorPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package simuladorPagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
